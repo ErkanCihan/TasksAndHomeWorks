565bdd331cf1629618b2f6611493f151fc8e0909
Erkan Cihan YILMAZ <therkancihan@gmail.com>
2022-11-28 21:24:59 +0300

    ErkanCihan tarafından oluşturuldu

---
+++ src/day35/Kaplan.java
@@ -0,0 +1,14 @@
+
+package day35;
+
+public class Kaplan extends Hayvan implements Avci,Yuzucu{
+    @Override
+    public void avlan() {
+        System.out.println("Kaplan avlanıyor.");
+    }
+
+    @Override
+    public void yuz() {
+        System.out.println("Kaplan yüzüyor.");
+    }
+}
